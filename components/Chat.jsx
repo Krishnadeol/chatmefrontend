dfc3173d46e323d1d912cc0551c3179290d4cafa
Krishnadeol <dps.krishnadeol.12a@gmail.com>
2024-06-16 22:52:54 +0530

    1 change

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -9,7 +9,7 @@ function Chat({ chat, user }) {
   useEffect(() => {
     const fetchMessages = async () => {
       const response = await axios.post(
-        "http://localhost:5000/message/getmsg",
+        "https://chatappbackup.onrender.com/message/getmsg",
         {
           from: user._id,
           to: chat._id,
@@ -21,11 +21,14 @@ function Chat({ chat, user }) {
   }, [chat]);
 
   const handleSendMsg = async (msg) => {
-    const response = await axios.post("http://localhost:5000/message/addmsg", {
-      from: user._id,
-      to: chat._id,
-      message: msg,
-    });
+    const response = await axios.post(
+      "https://chatappbackup.onrender.com/message/addmsg",
+      {
+        from: user._id,
+        to: chat._id,
+        message: msg,
+      }
+    );
   };
   console.log(chat);
   return (
@@ -65,4 +68,75 @@ function Chat({ chat, user }) {
   );
 }
 export default Chat;
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 80% 10%;
+  gap: 0.1rem;
+  overflow: hidden;
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    grid-template-rows: 15% 70% 15%;
+  }
+  .chat-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+    .user-details {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+  }
+  .chat-messages {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .message {
+      display: flex;
+      align-items: center;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+          max-width: 70%;
+        }
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
+      }
+    }
+  }
+`;
